6ecde6f0bf02566740b2d305ff60e5211bba6523
malte18 <malte.sielski@icloud.com>
2019-04-13 19:28:35 +0200

    update

--- health-app/components/Navigation/Navigation.js
+++ health-app/components/Navigation/Navigation.js
@@ -7,61 +7,8 @@ import { Button } from "yoda-design-system";
 
 const FOAF = new rdf.Namespace("http://xmlns.com/foaf/0.1/");
 
-class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      webId: props.webId,
-      friendToAdd: "",
-      canAddFriend: false
-    };
-  }
-
-  changeFriendToAdd(e) {
-    var xhr = new XMLHttpRequest();
-    const url = e.target.value;
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          this.setState({
-            friendToAdd: url,
-            canAddFriend: true
-          });
-        } else {
-          this.setState({
-            canAddFriend: false
-          });
-        }
-      }
-    };
-    const urlRegExp = new RegExp(/(\w+(:\/\/){1})(\w+\.)(\w+\.)(\w+\/)+/g);
-    if (urlRegExp.test(e.target.value)) {
-      xhr.open("GET", url);
-      xhr.send();
-    }
-  }
-
-  addFriend() {
-    let webId = this.props.webId;
-    let friendToAdd = this.state.friendToAdd;
-    console.log(friendToAdd);
-    const store = rdf.graph();
-    const updater = new rdf.UpdateManager(store);
-
-    let del = [];
-    let ins = rdf.st(
-      rdf.sym(webId),
-      FOAF("knows"),
-      rdf.sym(friendToAdd),
-      rdf.sym(webId).doc()
-    );
-    updater.update(del, ins, (ok, uri, message) => {
-      if (ok) console.log("Changes have been applied, reload page to see them");
-      else alert(message);
-    });
-  }
-
-  render() {
+const Navigation = () => {
+  
     return (
       <div style={{ padding: "3%" }}>
         <Navbar bg="light" variant="light" fixed="top">
@@ -70,30 +17,15 @@ class Navigation extends React.Component {
             <span style={{ marginLeft: "5%" }}>Solid Web</span>
           </Navbar.Brand>
           <Nav className="mr-auto">
-            <NavLink to="/" style={{ color: "#000", marginLeft: "10%" }}>
-              Profile
-            </NavLink>
-            <NavLink
-              to="/contacts"
-              style={{ color: "#000", marginLeft: "10%" }}
-            >
-              Contacts
-            </NavLink>
             <NavLink 
               to="/healthcase" 
               style={{ color: "#000", marginLeft: "10%" }}>
               Healthcase
             </NavLink>
           </Nav>
-          {this.props.webId ? (
-            ""
-          ) : (
-            <Button onClick={this.props.login}>Login</Button>
-          )}
         </Navbar>
       </div>
     );
   }
-}
 
 export default Navigation;
