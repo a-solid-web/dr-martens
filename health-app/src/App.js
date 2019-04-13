8695d634ddf4c61d03541072bfc6364f4b1899f9
elliott <elliottbrunet@gmail.com>
2019-04-13 21:58:24 +0200

    modal merge

--- health-app/src/App.js
+++ health-app/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route , Switch} from 'react-router-dom';
@@ -7,26 +6,10 @@ import Start from './components/start/Start';
 import GetUser from './components/GetUser/GetUser';
 
 class App extends React.Component {
-=======
-import React, { Component } from "react";
-import "./App.css";
-import ModalNotificationView from "./components/ModalNotificationView";
-import Button from "react-bootstrap/Button";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalShow: false
-    };
-  }
-
->>>>>>> origin/feature/modal-notefication-view
   render() {
     let toggleModal = () => this.setState({ modalShow: !this.state.modalShow });
 
     return (
-<<<<<<< HEAD
      <BrowserRouter>
       <div>
         <Navigation />
@@ -34,14 +17,6 @@ class App extends Component {
           <Route path="/" render={() => <Start /> } exact />
           <Route path="/getuser" render= {() => <GetUser /> } exact /> 
         </Switch>
-=======
-      <div className="App">
-        <Button onClick={toggleModal}>toggle</Button>
-        <ModalNotificationView
-          show={this.state.modalShow}
-          onHide={toggleModal}
-        />
->>>>>>> origin/feature/modal-notefication-view
       </div>
     </BrowserRouter>  
     );
--- health-app/src/components/start/Start.js
+++ health-app/src/components/start/Start.js
@@ -1,26 +1,45 @@
-import React from "react"; 
+import React, { Component } from "react"; 
 import HeroSection from '../HeroSection/HeroSection';
+import ModalNotificationView from '../ModalNotification/ModalNotificationView';
 import {Container, Button, Row} from "react-bootstrap";
 
 
-const Start = () => {
-    return (
-        <Container>
-            <HeroSection/> 
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                <Row>
-                    <h1>
-                        Send me your X-Ray Data!
-                    </h1>
-                </Row>
-            </div>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}> 
-                <Row>
-                    <Button variant="primary">Give Permission</Button>
-                </Row>
-            </div>
-        </Container>
-    )
+class Start extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          modalShow: false
+        };
+      }    
+
+    render() {
+
+        let toggleModal = () => this.setState({ modalShow: !this.state.modalShow });
+
+        return (
+            <Container>
+                <HeroSection/> 
+                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                    <Row>
+                        <h1>
+                            Send me your X-Ray Data!
+                        </h1>
+                    </Row>
+                </div>
+                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}> 
+                    <Row>
+                        <Button variant="primary" onClick={toggleModal}>Give Permission</Button>
+                    </Row>
+                </div>
+                <ModalNotificationView
+                    show={this.state.modalShow}
+                    onHide={toggleModal}
+                />
+            </Container>
+        )
+    }
+    
 }
 
 export default Start; 
