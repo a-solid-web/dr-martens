afafdcb0441857fe46bf02a1ef77131a5c27361f
elliott <elliottbrunet@gmail.com>
2019-04-13 21:38:23 +0200

    comit

--- health-app/src/components/start/Start.js
+++ health-app/src/components/start/Start.js
@@ -1,9 +1,5 @@
 import React from "react"; 
 import HeroSection from '../HeroSection/HeroSection';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from 'react-bootstrap/Container';
-
 
 const Start = () => {
     return (
