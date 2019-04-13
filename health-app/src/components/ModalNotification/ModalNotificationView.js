b33f2c70fe474e401154af1506be1c437938e724
elliott <elliottbrunet@gmail.com>
2019-04-13 21:58:54 +0200

    commit

---
+++ health-app/src/components/ModalNotification/ModalNotificationView.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+class ModalNotificationView extends React.Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Requested Permissions
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>hi</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+export default ModalNotificationView;
